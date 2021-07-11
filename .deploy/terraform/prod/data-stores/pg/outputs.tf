5f82dba712f5ab00b7990a00397ff235d3f14149
Abdi <abdi.cg.ahmed@gmail.com>
2021-07-11 15:53:20 +0100

    update output vars

--- .deploy/terraform/prod/data-stores/pg/outputs.tf
+++ .deploy/terraform/prod/data-stores/pg/outputs.tf
@@ -1,9 +1,9 @@
 output "prod_db_address" {
-  value = module.mysql_data_store.address
+  value = module.pgsql_data_store.address
   description = "Connect to the database at this endpoint"
 }
 
 output "prod_db_port" {
-  value = module.mysql_data_store.port 
+  value = module.pgsql_data_store.port 
   description = "The port the database is listening on"
 }
