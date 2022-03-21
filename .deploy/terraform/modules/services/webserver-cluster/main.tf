c29269c4f7ff55fbeeb6732f70d0426803bd6cf5
Abdi <abdi.cg.ahmed@gmail.com>
2022-03-21 14:56:40 +0000

    update

--- .deploy/terraform/modules/services/webserver-cluster/main.tf
+++ .deploy/terraform/modules/services/webserver-cluster/main.tf
@@ -121,20 +121,18 @@ resource "aws_iam_role" "logs_role" {
   name = "ec2-cloudwatch-2"
   assume_role_policy = <<EOF
   {
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": [
-          "ec2.amazonaws.com"
-        ]
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
+      "Version": "2012-10-17",
+      "Statement": [
+          {
+              "Action": "sts:AssumeRole",
+              "Principal": {
+                "Service": "ec2.amazonaws.com"
+              },
+              "Effect": "Allow",
+              "Sid": ""
+          }
+      ]
+  }
   EOF
 }
 
